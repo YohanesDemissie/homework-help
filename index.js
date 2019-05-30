e29cbd2772650ce3dd71a85c6b587b135770d5bb
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-05-30 02:50:27 -0700

    everything working as expected. Could use some profesional touch ups but weecerything meets standard at this point!

--- index.js
+++ index.js
@@ -31,25 +31,40 @@ var element = "<div>";
 let count = 0;
 function counter() {
   count += 1;
-  document.getElementById("counter").innerHTML = count + "/6 tries left";
-  if(count === 7) {
+  document.getElementById("counter").innerHTML = count + "/6 tries used";
+  if(count === 6) {
     alert("sorry you ran out of tries. Try again!");
     //RELOADS entire page
     location.reload();
   }
   console.log(count)
 }
+
+let hitters = 0
+function countHits() {
+  hitters += 1;
+  document.getElementById("hits").innerHTML = hitters + "/3 boats hit!";
+  if (hitters === 3) {
+    alert("CONGRATULATIONS YOU WON A FREE BATTLESHIP! CLAIM YOUR REWARD HERE AT 1-800-555-5555");
+    //RELOADS entire page
+    location.reload();
+  }
+  console.log(hitters)
+}
+
+
 for (let i = 0; i < squares; i++) {
   if (tiles[i] === "tiles") {
     element += "<button class=" + "tiles" + " onClick=" + "counter();" + "this.style.backgroundColor='red';" + ">"
   + tiles[i] + "</button>";
-    } else if(tiles[i] === 'battleship') {
-      element += "<button class=" + "tiles" + " onClick=" + " this.style.backgroundColor='green'" + ">"
+    } else if (tiles[i] === "battleship") {
+    element += "<button class=" + "tilesHit" + " onClick=" + " countHits();" + " this.style.backgroundColor='green';" + ">"
       + tiles[i] + "</button>";
   }
   element += "</div>";
 }
-document.getElementById("counter").innerHTML = count
+document.getElementById("counter").innerHTML = count;
+document.getElementById("hits").innerHTML = hitters;
 document.getElementById("demo").innerHTML = element;
 
 
